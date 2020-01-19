57def0dd1602dd56c2b805117e245bd336dbccf9
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 04:09:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function wakeDog(dogName, dogBreed) {
+  let activity = `Wake ${dogName} the ${dogBreed}`;
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return activity;
+}
+function leashDog(dogName, dogBreed) {
+  let activity = `Leash ${dogName} the ${dogBreed}`;
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return activity;
+}
+function walkToPark(dogName, dogBreed) {
+  let activity = `Walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return activity;
+}
+function throwFrisbee(dogName, dogBreed) {
+  let activity = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return activity;
+}
+function walkHome(dogName, dogBreed) {
+  let activity = `Walk home with ${dogName} the ${dogBreed}`;
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return activity;
+}
+function unleashDog(dogName, dogBreed) {
+  let activity = `Unleash ${dogName} the ${dogBreed}`;
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return activity;
+}
